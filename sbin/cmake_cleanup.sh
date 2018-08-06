ef87ac1fa4120d50c2e880553c5d099b8865116a
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2018-08-06 15:53:37 +0200

    no message

---
+++ sbin/cmake_cleanup.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+rm CMakeCache.txt
+rm -rf CMakeFiles/
+rm CTestTestfile.cmake 
+rm -rf cmake-build-debug
+rm Makefile
+rm cmake_install.cmake
+rm -rf tests/CMakeFiles/
+rm tests/CTestTestfile.cmake 
+rm tests/cmake_install.cmake 
+rm tests/Makefile
