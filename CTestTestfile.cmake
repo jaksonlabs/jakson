336b9266e91eb8604c9f93c7616729825f2dc6c4
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2018-08-06 15:40:06 +0200

    no message

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/marcus/git/ng5
-# Build directory: /Users/marcus/git/ng5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(tests)
--- tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/marcus/git/ng5/tests
-# Build directory: /Users/marcus/git/ng5/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(stdx_test_string_id_map "stdx_test_string_id_map")
