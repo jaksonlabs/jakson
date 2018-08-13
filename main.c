e3435f6e1a7fbb5aa8b47cd9b0ff6450df05d7d7
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2018-08-13 12:37:47 +0200

    no message

--- main.c
+++ main.c
@@ -65,88 +65,115 @@ struct vector *to_string_list(const char *contents)
 
 int main()
 {
-    timestamp_t read_begin = time_current_time_ms();
-    //char *contents = read_contents("/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-15pc-stringlist.txt");
-    char *contents = read_contents("/home/pinnecke/datasets/yago1/stringlists/yago1-11pc-stringlist.txt");
-    timestamp_t read_end = time_current_time_ms();
-    fprintf(stderr, "... %fsec\n", (read_end - read_begin) / 1000.0f);
+    printf("yago_percent;sample;num_buckets;time_created_sec;time_inserted_sec;time_bulk_sum_created_inserted;num_strings\n");
 
-    timestamp_t convert_begin = time_current_time_ms();
-    struct vector *lines = to_string_list(contents);
-    timestamp_t convert_end = time_current_time_ms();
-    fprintf(stderr, "... %fsec\n", (convert_end - convert_begin) / 1000.0f);
-    fflush(stderr);
+    const char *paths[11];/*
+    paths[0] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-11pc-stringlist.txt";
+    paths[1] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-19pc-stringlist.txt";
+    paths[2] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-27pc-stringlist.txt";
+    paths[3] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-35pc-stringlist.txt";
+    paths[4] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-43pc-stringlist.txt";
+    paths[5] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-51pc-stringlist.txt";
+    paths[6] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-59pc-stringlist.txt";
+    paths[7] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-67pc-stringlist.txt";
+    paths[8] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-75pc-stringlist.txt";
+    paths[9] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-83pc-stringlist.txt";
+    paths[10] = "/Volumes/PINNECKE EXT/science/datasets/yago/datasets/rdf3x/yago1.n3/samples-stringlist/yago1-91pc-stringlist.txt";
+ */
+    paths[0] = "/home/pinnecke/datasets/yago1/stringlists/yago1-11pc-stringlist.txt";
+    paths[1] = "/home/pinnecke/datasets/yago1/stringlists/yago1-19pc-stringlist.txt";
+    paths[2] = "/home/pinnecke/datasets/yago1/stringlists/yago1-27pc-stringlist.txt";
+    paths[3] = "/home/pinnecke/datasets/yago1/stringlists/yago1-35pc-stringlist.txt";
+    paths[4] = "/home/pinnecke/datasets/yago1/stringlists/yago1-43pc-stringlist.txt";
+    paths[5] = "/home/pinnecke/datasets/yago1/stringlists/yago1-51pc-stringlist.txt";
+    paths[6] = "/home/pinnecke/datasets/yago1/stringlists/yago1-59pc-stringlist.txt";
+    paths[7] = "/home/pinnecke/datasets/yago1/stringlists/yago1-67pc-stringlist.txt";
+    paths[8] = "/home/pinnecke/datasets/yago1/stringlists/yago1-75pc-stringlist.txt";
+    paths[9] = "/home/pinnecke/datasets/yago1/stringlists/yago1-83pc-stringlist.txt";
+    paths[10] = "/home/pinnecke/datasets/yago1/stringlists/yago1-91pc-stringlist.txt";
+
+
+    int yago_percent[11] = {
+        11,
+        19,
+        27,
+        35,
+        43,
+        51,
+        59,
+        67,
+        75,
+        83,
+        91
+    };
+
+    for (int pi = 0; pi < 11; pi++) {
+
+        timestamp_t read_begin = time_current_time_ms();
+        char *contents = read_contents(paths[pi]);
+        fprintf(stderr, "processing '%s'\n", paths[pi]);
+        timestamp_t read_end = time_current_time_ms();
+        fprintf(stderr, "... %fsec\n", (read_end - read_begin) / 1000.0f);
+
+        timestamp_t convert_begin = time_current_time_ms();
+        struct vector *lines = to_string_list(contents);
+        timestamp_t convert_end = time_current_time_ms();
+        fprintf(stderr, "... %fsec\n", (convert_end - convert_begin) / 1000.0f);
+        fflush(stderr);
+
+
+        struct string_dic dic;
+
+
+        for (int bucket_size = 1; bucket_size <= 4000; bucket_size += 160) {
+            for (int sample = 0; sample<NUM_SAMPLES; sample++) {
+
+                float created_duration = 0;
+                float insert_duration  = 0;
+
+
+                fprintf(stderr, "*** %d of %d in progress ***\n", sample+1, NUM_SAMPLES);
+                size_t num_buckets = bucket_size / 4000.0f * vector_len(lines);
+
+
+                timestamp_t create_begin = time_current_time_ms();
+                string_dic_create_naive(&dic, vector_len(lines), num_buckets , 10, 8, NULL);
+                timestamp_t create_end = time_current_time_ms();
+                created_duration = (create_end-create_begin) / 1000.0f;
+
+                string_id_t* ids; //, * ids_out;
+
+                char** strings = (char**) vector_data(lines);
+                size_t num_strings = vector_len(lines)-1;
+
+                string_dic_reset_counters(&dic);
+                timestamp_t inserted_begin = time_current_time_ms();
+                string_dic_insert(&dic, &ids, strings, num_strings);
+                timestamp_t inserted_end = time_current_time_ms();
+                insert_duration = (inserted_end-inserted_begin) / 1000.0f;
+
+                //struct string_lookup_counters counters;
+                //string_dic_counters(&counters, &dic);
+
+                string_dic_free(&dic, ids);
+
+                printf("%d;%d;%zu;%f;%f;%f;%zu\n", yago_percent[pi], sample, num_buckets, created_duration, insert_duration, (created_duration + insert_duration), vector_len(lines));
+
+                fflush(stderr);
+                fflush(stdout);
+            }
+        }
 
 
 
-    struct string_dic dic;
-
-    float created_agg = 0;
-    float insert_agg  = 0;
-
-    for (int sample = 0; sample < NUM_SAMPLES; sample++) {
-        printf("*** %d of %d in progress ***\n", sample + 1, NUM_SAMPLES);
-
-        timestamp_t create_begin = time_current_time_ms();
-        string_dic_create_naive(&dic, 10646182, 10.0*vector_len(lines), 10, 8, NULL);
-        timestamp_t create_end = time_current_time_ms();
-        created_agg += (create_end-create_begin);
-
-        string_id_t* ids; //, * ids_out;
-
-        char** strings = (char**) vector_data(lines);
-        size_t num_strings = vector_len(lines)-1;
-
-        string_dic_reset_counters(&dic);
-        timestamp_t inserted_begin = time_current_time_ms();
-        string_dic_insert(&dic, &ids, strings, num_strings);
-        timestamp_t inserted_end = time_current_time_ms();
-        insert_agg += (inserted_end-inserted_begin);
-
-        struct string_lookup_counters counters;
-        string_dic_counters(&counters, &dic);
-
-        printf("num_bucket_search_miss;%zu\n", counters.num_bucket_search_miss);
-        printf("num_bucket_search_hit;%zu\n", counters.num_bucket_search_hit);
-        printf("num_bucket_cache_search_miss;%zu\n", counters.num_bucket_cache_search_miss);
-        printf("num_bucket_cache_search_hit;%zu\n", counters.num_bucket_cache_search_hit);
-
-       // char **strings_ex = string_dic_extract(&dic, ids, num_strings);
-        // for (size_t i = 0; i < num_strings; i++) {
-        //    printf("[%zu] -> { '%s' }\n", ids[i], strings_ex[i]);
-        // }
-
-        string_dic_free(&dic, ids);
-
+        free(contents);
+        vector_drop(lines);
+        free(lines);
 
     }
 
-    fprintf(stderr, "created: ... %fsec\n", created_agg / NUM_SAMPLES / 1000.0f);
-    fprintf(stderr, "inserted: ... %fsec\n",insert_agg / NUM_SAMPLES /1000.0f);
-    fprintf(stderr, "----------------------------------\nTOTAL.........: %fsec (incl. 2sec I/O)\n", (2 + (created_agg+insert_agg) / NUM_SAMPLES /1000.0f));
-
-//
-//    timestamp_t locate_begin = time_current_time_ms();
-//    string_dic_locate_fast(&ids_out, &dic, strings, num_strings);
-//    timestamp_t locate_end = time_current_time_ms();
-//    fprintf(stderr, "locate: ... %fsec\n", (locate_end - locate_begin) / 1000.0f);
-//
-//    timestamp_t extracted_begin = time_current_time_ms();
-//    string_dic_extract(&dic, ids_out, num_strings);
-//    timestamp_t extracted_end = time_current_time_ms();
-//    fprintf(stderr, "extracted: ... %fsec\n", (extracted_end - extracted_begin) / 1000.0f);
-
-    fflush(stderr);
 
 
-  //  string_dic_free(&dic, ids);
-  //  string_dic_free(&dic, ids_out);
-
-    free(contents);
-    vector_drop(lines);
-    free(lines);
-
-    //free (strings);
 
     return 0;
 }
