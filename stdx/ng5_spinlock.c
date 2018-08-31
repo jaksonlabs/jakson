85357773d9735e8f889addfcc53f77c1169a2ace
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2018-08-31 10:47:56 +0200

    no message

--- stdx/ng5_spinlock.c
+++ stdx/ng5_spinlock.c
@@ -1,4 +1,7 @@
 #include <stdx/ng5_spinlock.h>
+#include <stdx/ng5_time.h>
+
+#define SPINLOCK_TAG "spinlock"
 
 int spinlock_create(struct spinlock *spinlock)
 {
@@ -12,6 +15,9 @@ int spinlock_create(struct spinlock *spinlock)
 
 int spinlock_lock(struct spinlock *spinlock)
 {
+    trace(SPINLOCK_TAG, "request aquire spin lock%s", "...");
+
+    timestamp_t begin = time_current_time_ms();
     check_non_null(spinlock)
     if (!pthread_equal(spinlock->owning_thread, pthread_self())) {
         while(atomic_flag_test_and_set(&spinlock->lock))
@@ -19,6 +25,10 @@ int spinlock_lock(struct spinlock *spinlock)
         /* remeber the thread that aquires this lock */
         spinlock->owning_thread = pthread_self();
     }
+    timestamp_t end = time_current_time_ms();
+
+    trace(SPINLOCK_TAG, "spin lock aquired after %f seconds", (end-begin)/1000.0f);
+
     return STATUS_OK;
 }
 
