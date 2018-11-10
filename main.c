2b5c9e577e47b0b4c9af0ad6e148a7de9307fd6f
Johannes Wünsche <johannes.wuensche97@gmail.com>
2018-11-10 22:26:57 +0100

    Add basic statistics

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include "src/strdic_sync.h"
-#include "src/strdic_async.h"
 #include "src/bitmap.h"
-#include "src/slicelist.h"
 #include "src/chunkreader.h"
+#include "src/slicelist.h"
+#include "src/strdic_async.h"
+#include "src/strdic_sync.h"
 #include "src/time.h"
+#include <slog.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 //#src <ng5/roadfire/roadfire.h>
-
-#define NUM_SAMPLES 1
-#define NTHREADS    (1)
 
 /*void roadfire_test() {
     struct storage_engine engine;
@@ -19,138 +17,192 @@
 
 #define NUM_SLICE_INSERT 100000
 
-void experiments_hashing()
-{
-    printf("chunk_num;sample;num_buckets;time_created_sec;time_inserted_sec;time_bulk_sum_created_inserted;num_strings_chunk;num_strings_total;num_distinct_strings\n");
+typedef struct {
+  float created_duration;
+  float insert_duration;
+  float total_duration;
+} stats;
 
-    const char* path = "/Volumes/PINNECKE EXT/science/cleaned_datasets/dbpedia-cleaned.txt";
-    //const char* path = "/home/pinnecke/datasets/yago1/stringlists/yago1-15pc-stringlist.txt";
-    //const char* path = "/home/pinnecke/mnt/datasets/mag-cleaned.txt";
-    //const char* path = "/Users/marcus/temp/file.txt";
+void experiments_hashing() {
+  slog_info(0, "chunk_num;sample;num_buckets;time_created_sec;time_inserted_"
+               "sec;time_bulk_sum_created_inserted;num_strings_chunk;num_"
+               "strings_total;num_distinct_strings");
 
+  // const char* path = "/Volumes/PINNECKE
+  // EXT/science/cleaned_datasets/dbpedia-cleaned.txt";
+  slog_info(0, "Reading in environment");
 
-    StringDictionary dic;
+  FILE *statistics_file = fopen("statistics.csv", "w");
+  stats statistics = {0.0, 0.0, 0.0};
 
-    for (size_t num_buckets = 50000; num_buckets<=50000; num_buckets += 50000) {
-        for (int sample = 0; sample<NUM_SAMPLES; sample++) {
+  fprintf(statistics_file, "%s, %s, %s, %s\n", "thread_num", "created_duration",
+          "insert_duration", "total_time");
 
-            ChunkReader reader;
+  const char *path = getenv("NG5_SET");
+  if (path == NULL) {
+    slog_panic(0, "No dataset defined; Define NG5_SET");
+    exit(1);
+  }
+  // const char* path =
+  // "/home/pinnecke/datasets/yago1/stringlists/yago1-15pc-stringlist.txt"; const
+  // char* path = "/home/pinnecke/mnt/datasets/mag-cleaned.txt"; const char* path
+  // = "/Users/marcus/temp/file.txt";
 
-            /**
-             * Here occurs undefined behaviour on different systems. Problem is,
-             * that on Linux the compiler treats the result of the calculation as int 
-             * before casting it to size_t in the function call, but the result is too big
-             * for int. Therefore, an explicit size_t cast is required to build
-             * without werror
-             **/
-            ChunkReaderCreate(&reader, NULL, path, (size_t)5 * 1024 * 1024 * 1204);
+  const char *max_threads_string = getenv("NG5_THREADS");
+  if (max_threads_string == NULL) {
+    slog_panic(0, "No maximum threads defined; Define NG5_THREADS");
+    exit(1);
+  }
+  const size_t max_threads = (size_t)atoi(max_threads_string);
 
-            float created_duration = 0;
-            float insert_duration = 0;
+  const char *samples_string = getenv("NG5_SAMPLES");
+  if (samples_string == NULL) {
+    slog_panic(0, "No sample number defined; Define NG5_SAMPLES");
+  }
+  const size_t samples = (size_t)atoi(samples_string);
 
-            fprintf(stderr, "*** %d of %d in progress ***\n", sample+1, NUM_SAMPLES);
+  StringDictionary dic;
+  for (size_t num_threads = 1; num_threads <= max_threads; num_threads++) {
+    slog_info(0, "%s %zu %s\n", "PERFORMING SAMPLE WITH", num_threads,
+              "THREADS");
+    for (size_t num_buckets = 50000; num_buckets <= 50000;
+         num_buckets += 50000) {
+      for (size_t sample = 1; sample <= samples; sample++) {
 
-            fprintf(stderr, "create..\n");
+        slog_info(0, "PERFORMING SAMPLE %zu", sample);
 
-            Timestamp create_begin = TimeCurrentSystemTime();
-            StringDictionaryCreateAsync(&dic, 3720000, num_buckets, 3720000, NTHREADS,
-                                        NULL);                         // <--------------------------------------------
-            Timestamp create_end = TimeCurrentSystemTime();
-            created_duration = (create_end-create_begin)/1000.0f;
+        ChunkReader reader;
 
-            Timestamp next_begin = TimeCurrentSystemTime();
-            Vector ofType(char *) *vector;
+        /**
+         * Here occurs undefined behaviour on different systems. Problem is,
+         * that on Linux the compiler treats the result of the calculation as
+         *int before casting it to size_t in the function call, but the result
+         *is too big for int. Therefore, an explicit size_t cast is required to
+         *build without werror
+         **/
+        ChunkReaderCreate(&reader, NULL, path, (size_t)5 * 1024 * 1024 * 1024);
 
-            size_t chunk_num = 0;
-            size_t total_num = 0;
+        float created_duration = 0;
+        float insert_duration = 0;
 
-            while((vector = ChunkReaderNext(&reader))) {
-                Timestamp next_end = TimeCurrentSystemTime();
-                fprintf(stderr, "got next %zu lines in %f sec\n", vector!=NULL ? VectorLength(vector) : 0,
-                        (next_end-next_begin)/1000.0f);
+        slog_info(0, "*** %d of %d in progress ***", sample, samples);
+        slog_info(0, "create..");
 
-                StringId* ids = NULL, * ids_out;
-                UNUSED(ids_out);
+        Timestamp create_begin = TimeCurrentSystemTime();
+        StringDictionaryCreateAsync(&dic, 3720000, num_buckets, 3720000,
+                                    num_threads, NULL);
+        Timestamp create_end = TimeCurrentSystemTime();
+        created_duration = (create_end - create_begin) / 1000.0f;
 
-                char** strings = (char**) VectorData(vector);
-                size_t num_strings = VectorLength(vector)-1;
+        Timestamp next_begin = TimeCurrentSystemTime();
+        Vector ofType(char *) * vector;
 
-                fprintf(stderr, "insert..\n");
+        size_t chunk_num = 0;
+        size_t total_num = 0;
 
-                StringDictionaryResetCounters(&dic);
-                Timestamp inserted_begin = TimeCurrentSystemTime();
-                StringDictionaryInsert(&dic, &ids, strings, num_strings, 0);
-                Timestamp inserted_end = TimeCurrentSystemTime();
-                insert_duration = (inserted_end-inserted_begin)/1000.0f;
+        while ((vector = ChunkReaderNext(&reader))) {
+          Timestamp next_end = TimeCurrentSystemTime();
+          slog_info(0, "got next %zu lines in %f sec",
+                    vector != NULL ? VectorLength(vector) : 0,
+                    (next_end - next_begin) / 1000.0f);
 
-//                fprintf(stderr, "locate..\n");
-//
-//                string_dic_locate_fast(&ids_out, &dic, strings, num_strings);
-//                for (size_t i = 0; i<num_strings; i++) {
-//                    StringId id_created = ids[i];
-//                    StringId id_located = ids_out[i];
-//                    //debug("check", "[%s] -> %zu", strings[i], id_located);
-//                    PANIC_IF_WARGS(id_created!=id_located,
-//                            "mapping broken for string [%s] id '%zu': expected %zu, is %zu",
-//                            strings[i], i, id_created, id_located);
-//                    assert(id_created==id_located);
-//                }
-//
-//                fprintf(stderr, "extract..\n");
-//
-//                char** extracted_strings = string_dic_extract(&dic, ids, num_strings);
-//                for (size_t i = 0; i<num_strings; i++) {
-//                    char* extracted = extracted_strings[i];
-//                    char* given = strings[i];
-//                    PANIC_IF(strcmp(extracted, given)!=0, "extraction broken");
-//                    assert(strcmp(extracted, given)==0);
-//                    //     debug("extracted id=%zu -> string [%s]\n", ids[i], extracted);
-//                }
-//
-//                fprintf(stderr, "remove..\n");
+          StringId *ids = NULL, *ids_out;
+          UNUSED(ids_out);
 
-                //  string_dic_remove(&dic, ids, num_strings);
+          char **strings = (char **)VectorData(vector);
+          size_t num_strings = VectorLength(vector) - 1;
 
-                //struct string_lookup_counters counters;
-                //string_dic_counters(&counters, &dic);
+          slog_info(0, "insert..");
 
-                size_t num_distinct;
-                StringDictionaryNumDistinct(&num_distinct, &dic);
+          StringDictionaryResetCounters(&dic);
+          Timestamp inserted_begin = TimeCurrentSystemTime();
+          StringDictionaryInsert(&dic, &ids, strings, num_strings, 0);
+          Timestamp inserted_end = TimeCurrentSystemTime();
+          insert_duration = (inserted_end - inserted_begin) / 1000.0f;
 
-                total_num += VectorLength(vector);
+          //                fprintf(stderr, "locate..\n");
+          //
+          //                string_dic_locate_fast(&ids_out, &dic, strings,
+          //                num_strings); for (size_t i = 0; i<num_strings; i++)
+          //                {
+          //                    StringId id_created = ids[i];
+          //                    StringId id_located = ids_out[i];
+          //                    //debug("check", "[%s] -> %zu", strings[i],
+          //                    id_located);
+          //                    PANIC_IF_WARGS(id_created!=id_located,
+          //                            "mapping broken for string [%s] id
+          //                            '%zu': expected %zu, is %zu",
+          //                            strings[i], i, id_created, id_located);
+          //                    assert(id_created==id_located);
+          //                }
+          //
+          //                fprintf(stderr, "extract..\n");
+          //
+          //                char** extracted_strings = string_dic_extract(&dic,
+          //                ids, num_strings); for (size_t i = 0; i<num_strings;
+          //                i++) {
+          //                    char* extracted = extracted_strings[i];
+          //                    char* given = strings[i];
+          //                    PANIC_IF(strcmp(extracted, given)!=0,
+          //                    "extraction broken"); assert(strcmp(extracted,
+          //                    given)==0);
+          //                    //     debug("extracted id=%zu -> string
+          //                    [%s]\n", ids[i], extracted);
+          //                }
+          //
+          //                fprintf(stderr, "remove..\n");
 
-                printf("%zu;%d;%zu;%f;%f;%f;%zu;%zu;%zu\n", chunk_num++, sample, num_buckets, created_duration,
-                        insert_duration,
-                        (created_duration+insert_duration), VectorLength(vector), total_num, num_distinct);
+          //  string_dic_remove(&dic, ids, num_strings);
 
-                StringDictionaryFree(&dic, ids);
-                //string_dic_free(&dic, extracted_strings);
-                //string_dic_free(&dic, ids_out);
+          // struct string_lookup_counters counters;
+          // string_dic_counters(&counters, &dic);
 
-                for (size_t i = 0; i< VectorLength(vector); i++) {
-                    char* string = *VECTOR_GET(vector, i, char *);
-                    free(string);
-                }
-                VectorDrop(vector);
-                free(vector);
+          size_t num_distinct;
+          StringDictionaryNumDistinct(&num_distinct, &dic);
 
-            }
+          total_num += VectorLength(vector);
 
-            fflush(stderr);
-            fflush(stdout);
+          slog_info(0, "%zu;%d;%zu;%f;%f;%f;%zu;%zu;%zu", chunk_num++, sample,
+                    num_buckets, created_duration, insert_duration,
+                    (created_duration + insert_duration), VectorLength(vector),
+                    total_num, num_distinct);
+          statistics = (stats){statistics.created_duration + created_duration,
+                               statistics.insert_duration + insert_duration,
+                               statistics.total_duration + created_duration +
+                                   insert_duration};
 
-            ChunkReaderDrop(&reader);
-            StringDictionaryDrop(&dic);
+          StringDictionaryFree(&dic, ids);
+          // string_dic_free(&dic, extracted_strings);
+          // string_dic_free(&dic, ids_out);
 
+          for (size_t i = 0; i < VectorLength(vector); i++) {
+            char *string = *VECTOR_GET(vector, i, char *);
+            free(string);
+          }
+          VectorDrop(vector);
+          free(vector);
         }
-        exit(0);
+
+        ChunkReaderDrop(&reader);
+        StringDictionaryDrop(&dic);
+      }
+
+      statistics = (stats){statistics.created_duration / samples,
+                           statistics.insert_duration / samples,
+                           statistics.total_duration / samples};
+      fprintf(statistics_file, "%zu, %f, %f, %f\n", num_threads,
+              statistics.created_duration, statistics.insert_duration,
+              statistics.total_duration);
     }
+  }
+  fclose(statistics_file);
+  exit(0);
 }
 
-int main()
-{
-//roadfire_test();
-    experiments_hashing();
+int main() {
+  slog_init("logfile", NULL, 1, 0);
 
-    return 0;
+  experiments_hashing();
+
+  return 0;
 }
