3cf9d464aa9a00fa413d34783b68149e52cd588f
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-02-07 15:16:25 +0100

    Fix colors in comparison graph

--- comparison.r
+++ comparison.r
@@ -1,12 +1,13 @@
 descent <- read.csv("descent_statistics.csv")
 static <- read.csv("static_statistics.csv")
 
+primary <- "#0db464"
+secondary <- "#8A2BE2"
+
 pdf(file="comparison.pdf", height=8, width=16)
-plot(descent[,3], type="l", xlab="Chunk Number", ylab="Total Time in seconds")
-lines(static[,3], col="blue")
+plot(descent[,3], type="l", xlab="Chunk Number", ylab="Total Time in seconds", col=primary)
+lines(static[,3], col=secondary)
 
-legend("topleft", legend=c("Fixed Threads", "Adaptive Threads"), col=c("black", "blue"), lty=1:1)
-
-print(descent[,3])
+legend("topleft", legend=c("Adaptive Threads", "Fixed Threads"), col=c(primary, secondary), lty=1:1)
 
 dev.off()
