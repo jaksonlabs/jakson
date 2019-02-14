ff29e8a7a6de92fc5f4365e8b1f1078ace3b2974
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2019-02-14 19:01:07 +0100

    Revert

--- src/carbon/carbon-spinlock.c
+++ src/carbon/carbon-spinlock.c
@@ -23,14 +23,9 @@
 bool carbon_spinlock_init(carbon_spinlock_t *spinlock)
 {
     CARBON_NON_NULL_OR_ERROR(spinlock)
-#ifndef __STDC_NO_ATOMICS__
     atomic_flag_clear(&spinlock->lock);
-#else
-    spinlock->mutex = PTHREAD_MUTEX_INITIALIZER;
-#endif
 
     memset(&spinlock->owner, 0, sizeof(pthread_t));
-
 
     return true;
 }
@@ -40,13 +35,8 @@ bool carbon_spinlock_acquire(carbon_spinlock_t *spinlock)
     carbon_timestamp_t begin = carbon_time_now_wallclock();
     CARBON_NON_NULL_OR_ERROR(spinlock)
     if (!pthread_equal(spinlock->owner, pthread_self())) {
-
-#ifndef __STDC_NO_ATOMICS__
         while (atomic_flag_test_and_set(&spinlock->lock));
-#else
-        pthread_mutex_lock(&spinlock->mutex);
-#endif
-        /** remember the thread that aquires this lock */
+        /** remeber the thread that aquires this lock */
         spinlock->owner = pthread_self();
     }
     carbon_timestamp_t end = carbon_time_now_wallclock();
@@ -61,13 +51,7 @@ bool carbon_spinlock_acquire(carbon_spinlock_t *spinlock)
 bool carbon_spinlock_release(carbon_spinlock_t *spinlock)
 {
     CARBON_NON_NULL_OR_ERROR(spinlock)
-
-#ifndef __STDC_NO_ATOMICS__
     atomic_flag_clear(&spinlock->lock);
-#else
-    pthread_mutex_lock(&mutex);
-#endif
-
     memset(&spinlock->owner, 0, sizeof(pthread_t));
     return true;
 }
