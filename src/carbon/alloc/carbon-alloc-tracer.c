91948da6b53034a191dd08f801cdbf9709864f57
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2019-03-25 16:41:10 +0100

    Add missing header

--- src/carbon/alloc/carbon-alloc-tracer.c
+++ src/carbon/alloc/carbon-alloc-tracer.c
@@ -15,6 +15,7 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <inttypes.h>
 #include "carbon/carbon-vector.h"
 
 #include "carbon/alloc/carbon-alloc_tracer.h"
