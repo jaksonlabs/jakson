ae7cfd694d213d777c9bddcfa7ba657ef1d94ed2
Oskar Kirmis <oskar.kirmis@posteo.de>
2019-07-07 16:39:12 +0200

    Added brute force implementation

--- include/carbon/compressor/auto/selector-brute-force.h
+++ include/carbon/compressor/auto/selector-brute-force.h
@@ -1,12 +1,23 @@
 #ifndef CARBON_COMPRESSOR_AUTO_SELECTOR_BRUTE_FORCE_H
 #define CARBON_COMPRESSOR_AUTO_SELECTOR_BRUTE_FORCE_H
 
-#include <carbon/carbon-compressor.h>
-#include <carbon/compressor/carbon-compressor-incremental.h>
+#include <carbon/compressor/auto/selector-common.h>
 
-carbon_compressor_t *carbon_compressor_find_by_strings_brute_force(
+typedef struct {
+    bool   sampling_enabled;
+    size_t sampling_block_count;
+    size_t sampling_block_length;
+    size_t sampling_min_entries;
+} carbon_compressor_selector_brute_force_config_t;
+
+void carbon_compressor_selector_brute_force_config_init(
+    carbon_compressor_selector_brute_force_config_t *config
+);
+
+carbon_compressor_selector_result_t carbon_compressor_find_by_strings_brute_force(
     carbon_vec_t ofType(char *) *strings,
-    carbon_doc_bulk_t const *context
+    carbon_doc_bulk_t const *context,
+    carbon_compressor_selector_brute_force_config_t const config
 );
 
 #endif
--- src/carbon/compressor/auto/selector-brute-force.c
+++ src/carbon/compressor/auto/selector-brute-force.c
@@ -7,29 +7,38 @@
 #include <carbon/carbon-memblock.h>
 
 
-typedef struct compressor_config {
-    carbon_compressor_incremental_prefix_type_e prefix;
-    carbon_compressor_incremental_prefix_type_e suffix;
-    bool reverse;
-    bool huffman;
-} compressor_config_t;
+// Avoid writing out these every time...
+static carbon_compressor_incremental_prefix_type_e const none = carbon_compressor_incremental_prefix_type_none;
+static carbon_compressor_incremental_prefix_type_e const inc = carbon_compressor_incremental_prefix_type_incremental;
+static carbon_compressor_incremental_prefix_type_e const table = carbon_compressor_incremental_prefix_type_table;
 
-static carbon_compressor_t *compressor_for(compressor_config_t config, carbon_doc_bulk_t const *context);
-static carbon_vec_t ofType(char *) *sample_strings(carbon_vec_t ofType(char *) *strings);
-static size_t compute_size(carbon_compressor_t *compressor, carbon_vec_t ofType(char *) *strings);
-static void sort_by_string(carbon_vec_t * entries, size_t idx_offset, size_t length, bool forward);
-static char const * get_strvec_str(carbon_vec_t * entries, size_t idx);
 
-carbon_compressor_t *carbon_compressor_find_by_strings_brute_force(
+// Forward decl of helpers
+static carbon_compressor_t *
+compressor_for(carbon_compressor_highlevel_config_t config, carbon_doc_bulk_t const *context);
+
+static carbon_vec_t ofType(char *) *
+sample_strings(carbon_vec_t ofType(char *) *strings, carbon_compressor_selector_brute_force_config_t const *config);
+
+static size_t
+compute_size(carbon_compressor_t *compressor, carbon_vec_t ofType(char *) *strings);
+
+static void
+sort_by_string(carbon_vec_t * entries, size_t idx_offset, size_t length, bool forward);
+
+static char const *
+get_strvec_str(carbon_vec_t * entries, size_t idx);
+
+
+
+carbon_compressor_selector_result_t carbon_compressor_find_by_strings_brute_force(
         carbon_vec_t ofType(char *) *strings,
-        carbon_doc_bulk_t const *context
+        carbon_doc_bulk_t const *context,
+        carbon_compressor_selector_brute_force_config_t const config
     ) {
     carbon_err_t err;
-    carbon_compressor_incremental_prefix_type_e const none = carbon_compressor_incremental_prefix_type_none;
-    carbon_compressor_incremental_prefix_type_e const inc = carbon_compressor_incremental_prefix_type_incremental;
-    carbon_compressor_incremental_prefix_type_e const table = carbon_compressor_incremental_prefix_type_table;
 
-    compressor_config_t configurations[] = {
+    carbon_compressor_highlevel_config_t configurations[] = {
         { .prefix = none,  .suffix = none,  .reverse = false, .huffman = false },
         { .prefix = none,  .suffix = none,  .reverse = false, .huffman = true  },
         { .prefix = none,  .suffix = none,  .reverse = true,  .huffman = false },
@@ -56,18 +65,28 @@ carbon_compressor_t *carbon_compressor_find_by_strings_brute_force(
         { .prefix = table, .suffix = inc,   .reverse = true,  .huffman = true  }
     };
 
-    carbon_vec_t ofType(char *) *samples = sample_strings(strings);
+    carbon_vec_t ofType(char *) *samples = sample_strings(strings, &config);
 
-    size_t              min_size   = SIZE_MAX;
-    compressor_config_t min_config = configurations[0];
+    carbon_compressor_selector_result_t result = { .estimated_size = SIZE_MAX };
+    carbon_huffman_encoder_create(&result.huffman);
 
     for(size_t i = 0; i < sizeof(configurations)/sizeof(configurations[0]);++i) {
         carbon_compressor_t * compressor = compressor_for(configurations[i], context);
 
         size_t const size = compute_size(compressor, samples);
-        if(size < min_size) {
-            min_size   = size;
-            min_config = configurations[i];
+        if(size < result.estimated_size) {
+
+            result.estimated_size = size;
+            result.config         = configurations[i];
+
+            if(result.config.huffman) {
+                carbon_compressor_incremental_extra_t *extra =
+                        (carbon_compressor_incremental_extra_t *)compressor->extra;
+
+                carbon_huffman_encoder_drop(&result.huffman);
+                carbon_huffman_encoder_create(&result.huffman);
+                carbon_huffman_encoder_clone(&extra->huffman_encoder, &result.huffman);
+            }
         }
 
         carbon_compressor_drop(&err, compressor);
@@ -76,45 +95,44 @@ carbon_compressor_t *carbon_compressor_find_by_strings_brute_force(
     CARBON_CONSOLE_WRITELN(
                 stdout,
                 "            Detected settings: prefix = %s, suffix = %s, huffman = %s, rev_str = %s",
-                (min_config.prefix == inc ? "incremental" : ( min_config.prefix == table ? "table" : "none" )),
-                (min_config.suffix == inc ? "incremental" : ( min_config.suffix == table ? "table" : "none" )),
-                min_config.huffman ? "true" : "false",
-                min_config.reverse ? "true" : "false"
+                (result.config.prefix == inc ? "incremental" : ( result.config.prefix == table ? "table" : "none" )),
+                (result.config.suffix == inc ? "incremental" : ( result.config.suffix == table ? "table" : "none" )),
+                result.config.huffman ? "true" : "false",
+                result.config.reverse ? "true" : "false"
     );
 
     carbon_vec_drop(samples);
     free(samples);
 
-    return compressor_for(min_config, context);
+    result.compressor = compressor_for(result.config, context);
+    return result;
 }
 
 static carbon_vec_t ofType(char *) *sample_strings(
-        carbon_vec_t ofType(char *) *strings
+        carbon_vec_t ofType(char *) *strings,
+        carbon_compressor_selector_brute_force_config_t const *config
     ) {
-    size_t const block_min   = 32;
-    size_t const block_size  = 16;
-    size_t const threshold   = 1000;
-    size_t const entries_min = block_min * block_size;
     size_t const block_cnt   =
-            strings->num_elems <= threshold ?
-                block_min : (size_t)(strings->num_elems * (1.0 / entries_min));
+            strings->num_elems <= config->sampling_min_entries ?
+                config->sampling_block_count :
+                (size_t)(strings->num_elems * config->sampling_block_count / (double)config->sampling_min_entries);
 
     carbon_vec_t ofType(char *) *samples = malloc(sizeof(carbon_vec_t));
     carbon_vec_create(
-                samples, NULL, sizeof(char *), block_size * block_cnt
+                samples, NULL, sizeof(char *), config->sampling_block_length * block_cnt
     );
 
-    if(strings->num_elems <= block_size * block_cnt) {
+    if(!config->sampling_enabled || strings->num_elems <= config->sampling_min_entries) {
         // Copy all strings
         carbon_vec_push(samples, carbon_vec_at(strings, 0), strings->num_elems);
     } else {
         // Sample strings
         for(size_t i = 0; i < block_cnt; ++i) {
             size_t const start_idx =
-                    (size_t)((size_t)rand() % (strings->num_elems - block_size));
+                    (size_t)((size_t)rand() % (strings->num_elems - config->sampling_block_length));
 
             carbon_vec_push(
-                        samples, carbon_vec_at(strings, start_idx), block_size
+                        samples, carbon_vec_at(strings, start_idx), config->sampling_block_length
             );
         }
     }
@@ -124,7 +142,7 @@ static carbon_vec_t ofType(char *) *sample_strings(
 
 
 static carbon_compressor_t *compressor_for(
-        compressor_config_t config,
+        carbon_compressor_highlevel_config_t config,
         carbon_doc_bulk_t const *context
     ) {
     carbon_compressor_t *compressor = malloc(sizeof(carbon_compressor_t));
@@ -133,22 +151,29 @@ static carbon_compressor_t *compressor_for(
 
     carbon_err_t err;
     carbon_compressor_by_type(&err, compressor, context, CARBON_COMPRESSOR_INCREMENTAL);
+
+    ////// Apply the provided compressor_config_t to the compressor
     carbon_compressor_set_option(
         &err, compressor, "prefix",
-        config.prefix == carbon_compressor_incremental_prefix_type_incremental ? "incremental" : (
-            config.prefix == carbon_compressor_incremental_prefix_type_table ? "table" : "none"
-        )
+        config.prefix == inc ? "incremental" : (config.prefix == table ? "table" : "none")
     );
     carbon_compressor_set_option(
         &err, compressor, "suffix",
-        config.suffix == carbon_compressor_incremental_prefix_type_incremental ? "incremental" : "none"
+        config.suffix == inc ? "incremental" : "none"
     );
     carbon_compressor_set_option(
-        &err, compressor, "huffman",
-        config.huffman ? "true" : "false"
+        &err, compressor, "huffman", config.huffman ? "true" : "false"
     );
 
-    snprintf(tmp_buffer, 15, "%lu", 20l);
+    carbon_compressor_set_option(
+        &err, compressor, "reverse_strings", config.reverse ? "true" : "false"
+    );
+
+    carbon_compressor_set_option(
+        &err, compressor, "reverse_sort", config.reverse ? "true" : "false"
+    );
+
+    snprintf(tmp_buffer, 15, "%lu", 100l);
     carbon_compressor_set_option(
         &err, compressor, "delta_chunk_length", tmp_buffer
     );
@@ -214,4 +239,14 @@ static char const * get_strvec_str(
         size_t index
     ) {
     return *(char const * const *)carbon_vec_at(entries, index);
+}
+
+void carbon_compressor_selector_brute_force_config_init(
+        carbon_compressor_selector_brute_force_config_t *config
+    )
+{
+    config->sampling_enabled = true;
+    config->sampling_block_count = 16;
+    config->sampling_block_length = 32;
+    config->sampling_min_entries = 1000;
 }
