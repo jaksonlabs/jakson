9b4c3e928512b71b35e971fd06fc6daf36464d65
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2019-09-27 15:22:35 +0200

    add test

---
+++ tests/carbon/test-abstract-types.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <printf.h>
+
+#include <jakson/jakson.h>
+
+TEST(TestAbstractTypes, XXXX) {
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
